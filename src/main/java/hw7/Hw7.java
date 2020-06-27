910d743d5135a5d7a4e75904a18caacad0239447
Nina <nina.tuttle@yahoo.com>
2020-06-27 17:29:12 -0500

    hw7 commit

---
+++ src/main/java/hw7/Hw7.java
@@ -0,0 +1,23 @@
+package hw7;
+
+//public class Hw7 {
+//    public static void main(String[] args) {
+////    1. Create function `sum` - which takes int[] as a param and returns sum of all elements of the array.
+////                invoke it from main() - pass {2,3,5,6,7,8} as a param and print out result1.
+////                Create function `sum` - which takes int[] as a param and returns sum of all
+////                elements of the array.
+////                invoke it from main() - pass {2,3,5,6,7,8} as a param and print out result
+//
+//        int[] sum = {2, 3, 5, 6, 7, 8};
+//        System.out.println(sum[i]);
+////    }
+//
+//        public static int sum[] arr
+//        int sum = 0;
+//        for (int v : sum) {
+//            sum += sum + v;
+//        }
+//
+//        System.out.println(sum);
+//    }
+//
