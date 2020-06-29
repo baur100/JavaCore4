4702269ba661a3e3cb8d08722f9d47900b536e84
Rahul Reddy <rkr6149@gmail.com>
2020-06-29 08:56:49 -0700

    hw7

--- src/main/java/hw7/HW7.java
+++ src/main/java/hw7/HW7.java
@@ -1,45 +1,4 @@
 package hw7;
 
-import java.util.Arrays;
-
 public class HW7 {
-    public static void main(String[] args) {
-        //q1. Array Sum
-        int[] i1 = {1, 2, 6, 4, 8};
-        int sum = sum(i1);
-        System.out.println("\nSum of the array values: " + sum);
-        sort(i1);
-
-        int minofArray= min(i1);
-        System.out.println(minofArray);
-
-    }
-    public static int sum(int [] arrayofIntegerNumbers) {
-        int final=0;
-        for (int v : arrayofIntegerNumbers) {
-            final=final+v;
-        }
-        return final ;
-    }
-
-    public static void sort(int[]arrayToSortAndPrint){
-        Arrays.sort(arrayToSortAndPrint);
-        for (int v : arrayToSortAndPrint) {
-            System.out.println(v);
-        }
-        }
-
-        public static int min(int[]a){
-            int min=a[0];
-            for (int v:a);
-    {
-        if (v < min) {
-            min = v;
-        }
-    }
-            return min;
-                }
-    }
-
-
-
+}
