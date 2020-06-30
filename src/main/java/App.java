15654ba27dddd321c566007f55796e7a87ac594e
irinastukalova <irina_stukalova@hotmail.com>
2020-06-29 18:47:01 -0700

    hw9Object

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,18 +4,18 @@ public class App {
     public static void main(String[] args) {
 
 
-        int[] arr = {3, 9, 1, 8, 4, 8, 5};
-        System.out.println(arr);
-    }
-
-    public static int min(int[] arr) {
-        int min = arr[0];
-        for (int v : arr) {
-            if (v > min) {
-                min = v;
-            }
-        }
-return min;
+//        int[] arr = {3, 9, 1, 8, 4, 8, 5};
+//        System.out.println(v);
+//    }
+//
+//    public static int min(int[] arr) {
+//        int min = arr[0];
+//        for (int v : arr) {
+//            if (v > min) {
+//                min = v;
+//            }
+//        }
+//return min;
 
     }
 }
--- src/main/java/hw7conditions/Hw7App.java
+++ src/main/java/hw7conditions/Hw7App.java
@@ -1,6 +1,6 @@
 package hw7conditions;
 
-public class hw7App {
+public class Hw7App {
     public static void main(String[] args) {
         System.out.println("Hello Students!");
     }
