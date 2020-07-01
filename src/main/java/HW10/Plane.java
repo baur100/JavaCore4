6e9f2aaea0a6451de4246a5dfe7d61dc7d9b6350
adjaratou <daremy23@yahoo.com>
2020-07-01 00:34:05 -0700

    HW8

---
+++ src/main/java/HW10/Plane.java
@@ -0,0 +1,49 @@
+package HW10;
+
+public class Plane {
+    private int planenumber;
+    private String airlines;
+    private String make;
+
+    public Plane() {
+    }
+
+    public void setPlanenumber(int planenumber) {
+        this.planenumber = planenumber;
+    }
+
+    public void setAirlines(String airlines) {
+        this.airlines = airlines;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public int getPlanenumber() {
+        return planenumber;
+    }
+
+    public String getAirlines() {
+        return airlines;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public Plane(int planenumber, String airlines, String make) {
+        this.planenumber = planenumber;
+        this.airlines = airlines;
+        this.make = make;
+
+    }
+
+    public void printinfo() {
+        System.out.println("This plane number is " + planenumber + ".");
+        System.out.println("This plane's airlines is " + airlines + ".");
+        System.out.println("This plane's make is " + make + ".");
+    }
+
+}
+
