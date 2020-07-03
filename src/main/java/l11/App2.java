8fad52e5ca4bd80bb54d84c56fb4745b1549d628
Max <mlynnyk@outlook.com>
2020-07-03 12:41:34 -0500

    HomeWork10

--- src/main/java/l11/App2.java
+++ src/main/java/l11/App2.java
@@ -3,7 +3,7 @@ package l11;
 public class App2 {
     public static void main(String[] args) {
 
-        Address home = new Address("112 Holly st", "Bronx", "NY", 10112, true);
+        Address home = new Address("112 Holly st", "Bronx", "NY", 10112);
         String name = new String("Ted");
         String lastName = new String("Simmons");
         String subj = "Math";
