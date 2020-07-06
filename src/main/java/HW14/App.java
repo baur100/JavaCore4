30e5575706443ace7e1ed64558ab30728f67aa64
adjaratou <daremy23@yahoo.com>
2020-07-06 11:17:15 -0700

    HW14 second commit

--- src/main/java/HW14/App.java
+++ src/main/java/HW14/App.java
@@ -3,13 +3,28 @@ package HW14;
 public class App {
     public static void main(String[] args) {
         Doctor drjohn=new Doctor(1, "Salvatore", "Ferragamo", "10 Regent St", "Pediatrics");
-        System.out.println("\nHere is the Doctor's Information:");
+        System.out.println("\n First Doctor's Information:");
         drjohn.printinfo();
-        Nurse nursemary= new Nurse(8, "Mary", "Cooper", "105 Princeton Dr", "Surgery");
-        System.out.println("\nHere is the Nurse's Information:");
+
+        Doctor drellen=new Doctor(3, "Ellen", "Cornwell", "8 Jasmine Ave", "Labor & Delivery");
+        System.out.println("\n Second Doctor's Information:");
+        drjohn.printinfo();
+
+        Nurse nursemary= new Nurse(2, "Mary", "Cooper", "105 Princeton Dr", "Surgery");
+        System.out.println("\nFirst Nurse's Information:");
         nursemary.printinfo();
+
+        Nurse nursesally= new Nurse(5, "Sally", "Fields", "43 Camarillo Dr", "Dentistry");
+        System.out.println("\nFirst Nurse's Information:");
+        nursemary.printinfo();
+
+
         Janitor paul=new Janitor(13, "Paul", "Smith", "45 Adams Blvd", "First Floor");
-        System.out.println("\nHere is the Janitor's Information:");
+        System.out.println("\nFirst Janitor's Information:");
+        paul.printinfo();
+
+        Janitor christopher=new Janitor(13, "Christopher", "Warman", "4 Alexandria Ave", "Equipment");
+        System.out.println("\nSecond Janitor's Information:");
         paul.printinfo();
 
     }
