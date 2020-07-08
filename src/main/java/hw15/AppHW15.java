f32698a500424fbda22bf3e854bc75543b04c0c7
SinthiaA <sinthiaalam6@gmail.com>
2020-07-07 23:58:00 -0700

    HW15_July7th_updated

--- src/main/java/hw15/AppHW15.java
+++ src/main/java/hw15/AppHW15.java
@@ -4,6 +4,7 @@ public class AppHW15 {
     public static void main(String[] args) {
         Watermelon fruit1 = new Watermelon("Red", 15);
         System.out.println(fruit1.haveSeeds());
+        System.out.println("****************");
 
         Banana fruit2 = new Banana("Yellow", 1);
         System.out.println(fruit2.haveSeeds());
