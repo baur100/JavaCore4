531a733bfd4295fe73005eb07b029352d051544e
Viktoryia Simakova <p.s.victoryia1405@gmail.com>
2020-07-11 04:10:46 -0700

    HW16

--- src/main/java/hw16/Main.java
+++ src/main/java/hw16/Main.java
@@ -2,6 +2,7 @@ package hw16;
 
 import java.util.ArrayList;
 import java.awt.Point;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String [] args){
@@ -14,11 +15,11 @@ public class Main {
         colors.add("Green");
         colors.add("Purple");
         System.out.println("ArrayList \"colors\" contains : ");
-        System.out.println(colors.toArray());
+        System.out.println(Arrays.toString(colors.toArray()));
         System.out.println("First element : " + colors.get(0));
         System.out.println("Last element : " + colors.get(colors.size()-1));
         System.out.println("Swap first and last elements");
-        String temp = colors.get(colors.size()-1);
+        String temp = colors.get(0);
         colors.set(0,colors.get(colors.size()-1));
         colors.set(colors.size()-1, temp);
         System.out.println("First element : " + colors.get(0));
@@ -26,7 +27,7 @@ public class Main {
         System.out.println("Remove first and last elements");
         colors.remove(0);
         colors.remove(colors.size()-1);
-        System.out.println(colors.toArray());
+        System.out.println(Arrays.toString(colors.toArray()));
 
         ArrayList<Integer> numbers = new ArrayList<Integer>();
         numbers.add(11);
@@ -35,11 +36,11 @@ public class Main {
         numbers.add(64);
         numbers.add(24);
         System.out.println("ArrayList \"numbers\" contains : ");
-        System.out.println(numbers.toArray());
+        System.out.println(Arrays.toString(numbers.toArray()));
         System.out.println("First element : " + numbers.get(0));
-        System.out.println("Last element : " + colors.get(numbers.size()-1));
+        System.out.println("Last element : " + numbers.get(numbers.size()-1));
         System.out.println("Swap first and last elements");
-        int temp1 = numbers.get(numbers.size()-1);
+        int temp1 = numbers.get(0);
         numbers.set(0,numbers.get(numbers.size()-1));
         numbers.set(numbers.size()-1, temp1);
         System.out.println("First element : " + numbers.get(0));
@@ -47,7 +48,7 @@ public class Main {
         System.out.println("Remove first and last elements");
         numbers.remove(0);
         numbers.remove(numbers.size()-1);
-        System.out.println(numbers.toArray());
+        System.out.println(Arrays.toString(numbers.toArray()));
 
         ArrayList<Point> points = new ArrayList<Point>();
         Point point = new Point(1,2);
@@ -61,19 +62,29 @@ public class Main {
         point = new Point(5,5);
         points.add(point);
         System.out.println("ArrayList \"points\" contains : ");
-        System.out.println(points.toArray());
-        System.out.println("First element : " + points.get(0));
-        System.out.println("Last element : " + colors.get(points.size()-1));
+        for(Point point1 : points) {
+            System.out.println("(" + point1.getX() + "," + point1.getY()+")");
+        }
+        System.out.print("First element : " );
+        System.out.println("(" + points.get(0).getX() + "," + points.get(0).getY()+")");
+        System.out.print("Last element : ");
+        point = points.get(points.size()-1);
+        System.out.println("(" + point.getX() + "," + point.getY()+")");
         System.out.println("Swap first and last elements");
-        Point temp2 = points.get(points.size()-1);
+        Point temp2 = points.get(0);
         points.set(0,points.get(points.size()-1));
         points.set(points.size()-1, temp2);
-        System.out.println("First element : " + points.get(0));
-        System.out.println("Last element : " + points.get(points.size()-1));
+        System.out.print("First element : " );
+        System.out.println("(" + points.get(0).getX() + "," + points.get(0).getY()+")");
+        System.out.print("Last element : ");
+        point = points.get(points.size()-1);
+        System.out.println("(" + point.getX() + "," + point.getY()+")");
         System.out.println("Remove first and last elements");
-        points.remove(0);
         points.remove(colors.size()-1);
-        System.out.println(points.toArray());
+        points.remove(0);
+        for(Point point1 : points) {
+            System.out.println("(" + point1.getX() + "," + point1.getY()+")");
+        }
     }
 
 }
