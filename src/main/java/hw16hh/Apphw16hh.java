238ff498e9d61994425ce4e5612857b37ac8eb7f
Helen He <hellynyc@gmail.com>
2020-07-11 23:03:13 -0400

    hw16HH2

--- src/main/java/hw16hh/Apphw16hh.java
+++ src/main/java/hw16hh/Apphw16hh.java
@@ -7,39 +7,48 @@ import java.util.Map;
 
 public class Apphw16hh {
     public static void main(String[] args) {
-        Address brooklynHosAdd = new Address("121 DeKalb Ave", "Brooklyn", "NY", 11201);
 
-        Doctor guillaume = new Doctor("Melissa", "Guillaume", Title.CARDIOLOGIST);
-        Doctor abraham = new Doctor("Maria", "Abraham", Title.PEDIATRICIAN);
-        Doctor chai = new Doctor("Edward", "Chai", Title.NEUROLOGIST);
-        Doctor burger = new Doctor("Steven", "Burger", Title.SURGEON);
-        Doctor cuevas = new Doctor("Juana", "Cuevas", Title.OBGYN);
+        try {
+
+            Address brooklynHosAdd = new Address("121 DeKalb Ave", "Brooklyn", "NY", 11201);
+
+            Doctor guillaume = new Doctor("Melissa", "Guillaume", Title.CARDIOLOGIST);
+            Doctor abraham = new Doctor("Maria", "Abraham", Title.PEDIATRICIAN);
+            Doctor chai = new Doctor("Edward", "Chai", Title.NEUROLOGIST);Doctor burger = new Doctor("Steven", "Burger", Title.SURGEON);
+            Doctor cuevas = new Doctor("Juana", "Cuevas", Title.OBGYN);
 
 
-        List<Doctor> brooklynDoc = new ArrayList<>();
-        brooklynDoc.add(guillaume);
-        brooklynDoc.add(abraham);
-        brooklynDoc.add(chai);
-        brooklynDoc.add(burger);
-        brooklynDoc.add(cuevas);
+            List<Doctor> brooklynDoc = new ArrayList<>();
+            brooklynDoc.add(guillaume);
+            brooklynDoc.add(abraham);
+            brooklynDoc.add(chai);
+            brooklynDoc.add(burger);
+            brooklynDoc.add(cuevas);
 
-        Map<Integer, String> brooklynRooms = new HashMap<>();
-        brooklynRooms.put(806, "Geriatrics");
-        brooklynRooms.put(311, "Critical Care");
-        brooklynRooms.put(220, "Infectious Disease");
-        brooklynRooms.put(1109, "General Surgery");
-        brooklynRooms.put(612, "Pediatrics");
+            Map<Integer, String> brooklynRooms = new HashMap<>();
+            brooklynRooms.put(806, "Geriatrics");
+            brooklynRooms.put(311, "Critical Care");
+            brooklynRooms.put(220, "Infectious Disease");
+            brooklynRooms.put(1109, "General Surgery");
+            brooklynRooms.put(612, "Pediatrics");
 
-        List<InsCo> brooklynIns = new ArrayList<>();
-        brooklynIns.add(InsCo.AETNA);
-        brooklynIns.add(InsCo.UNITED);
-        brooklynIns.add(InsCo.CIGNA);
-        brooklynIns.add(InsCo.BLUECROSS);
-        brooklynIns.add(InsCo.EMBLEM);
-        brooklynIns.add(InsCo.KAISER);
+            List<InsCo> brooklynIns = new ArrayList<>();
+            brooklynIns.add(InsCo.AETNA);
+            brooklynIns.add(InsCo.UNITED);
+            brooklynIns.add(InsCo.CIGNA);
+            brooklynIns.add(InsCo.BLUECROSS);
+            brooklynIns.add(InsCo.EMBLEM);
+            brooklynIns.add(InsCo.KAISER);
 
-        Hospital brooklynHos = new Hospital("Brooklyn Hospital", brooklynHosAdd, brooklynDoc, brooklynRooms, brooklynIns);
+            Hospital brooklynHos = new Hospital("Brooklyn Hospital", brooklynHosAdd, brooklynDoc, brooklynRooms, brooklynIns);
 
-        System.out.println(brooklynHos.toString());
+            System.out.println(brooklynHos.toString());
+
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+        } finally {
+            System.out.println("**********************************************");
+            System.out.println("Yay! There are no errors in my code. :)");
+        }
     }
 }
