2141988e5cc2efc02fccfc1e982efee7aafa13ed
pavel <pavel.smoljaninov@yahoo.com>
2020-07-13 22:38:53 -0400

    hw16

--- src/main/java/hw16/Doctor.java
+++ src/main/java/hw16/Doctor.java
@@ -6,11 +6,55 @@ public class Doctor {
     private Position position;
     private InsuranceCompanies insuranceAccepted;
 
+
+
     public Doctor(String name, String lastName, Position position, InsuranceCompanies insuranceAccepted) {
         this.name = name;
         this.lastName = lastName;
         this.position = position;
         this.insuranceAccepted = insuranceAccepted;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public InsuranceCompanies getInsuranceAccepted() {
+        return insuranceAccepted;
+    }
+
+    public void setInsuranceAccepted(InsuranceCompanies insuranceAccepted) {
+        this.insuranceAccepted = insuranceAccepted;
+    }
+
+
+
+
+    public void doctorPrint() {
+
+        System.out.println(this.name+this.lastName+this.position+this.insuranceAccepted);
+
+    }
+
 }
 
--- src/main/java/hw16/Hospital.java
+++ src/main/java/hw16/Hospital.java
@@ -1,10 +1,7 @@
 package hw16;
 
 import java.nio.channels.UnresolvedAddressException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.RandomAccess;
+import java.util.*;
 
 public class Hospital {
 
@@ -16,14 +13,19 @@ public class Hospital {
         rooms.put(24, "Surgery");
         rooms.put(26, "Emergency");
 
-        ArrayList<InsuranceCompanies > acceptedInsuranses = new ArrayList<InsuranceCompanies>();
+        ArrayList<InsuranceCompanies> acceptedInsuranses = new ArrayList<InsuranceCompanies>();
         acceptedInsuranses.add(InsuranceCompanies.COSTCO);
         acceptedInsuranses.add(InsuranceCompanies.BLU_CROSS);
         acceptedInsuranses.add(InsuranceCompanies.GEICO);
 
-        Doctor number1 = new Doctor("Bob", "Smith", Position.PEDIATOR, InsuranceCompanies.GEICO );
-        Doctor number2 = new Doctor("Alex", "Marvin", Position.SURGEON, InsuranceCompanies.BLU_CROSS);
-        Doctor number3 = new Doctor( "Ana", "Silva", Position.NURSE, InsuranceCompanies.WALLMART);
+
+        System.out.println(acceptedInsuranses);
+        System.out.println(rooms);
+
+
+        Doctor number1 = new Doctor("Bob ", "Smith ", Position.PEDIATOR, InsuranceCompanies.GEICO);
+        Doctor number2 = new Doctor("Alex ", "Marvin ", Position.SURGEON, InsuranceCompanies.BLU_CROSS);
+        Doctor number3 = new Doctor("Ana ", "Silva ", Position.NURSE, InsuranceCompanies.WALLMART);
 
 
         List<Doctor> doctorsList = new ArrayList<>();
@@ -31,15 +33,19 @@ public class Hospital {
         doctorsList.add(number2);
         doctorsList.add(number3);
 
-        System.out.println(doctorsList);
 
-        System.out.println(acceptedInsuranses);
-        System.out.println(rooms);
-        for (Doctor v: doctorsList) {
-           System.out.println(v);
+        number1.doctorPrint();
+        number2.doctorPrint();
+        number3.doctorPrint();
 
-        }
+
+//        System.out.println(Arrays.toString(doctorsList.toArray()));
+//
+//        doctorsList.size();
+//
+//        for (Doctor v: doctorsList) {
+//           System.out.println(v); }
+   }
+
     }
-
-}
 
