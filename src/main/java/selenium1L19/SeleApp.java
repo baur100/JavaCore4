14e4837f51298648c779dd697e4d8f7fe97748a3
sindhorithulasiram <sindhorithulasiram@gmail.com>
2020-07-18 11:11:51 -0700

    Lesson19 Selenium pom.xml dependency

--- src/main/java/selenium1L19/SeleApp.java
+++ src/main/java/selenium1L19/SeleApp.java
@@ -8,14 +8,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleApp {
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","chromedriver");
+//        System.setProperty("webdriver.chrome.driver","package/Users/sindhorithulasiram/IdeaProjects/JavaCore4/chromedriver");
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://google.com");
+        Thread.sleep(5);
 
         WebElement searchField = driver.findElement(By.cssSelector("[name='q]"));
         searchField.sendKeys("JAVA");
         searchField.sendKeys(Keys.ENTER);
         Thread.sleep(5000);
+        driver.close();
     }
 }
