f1f3171650ef6be090736e8dfd330bc55d1c4441
Helen He <hellynyc@gmail.com>
2020-07-20 23:00:16 -0400

    HH pom update

--- src/main/java/s4/App.java
+++ src/main/java/s4/App.java
@@ -7,9 +7,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App {
-    public static <WebDriver> void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://google.com");
