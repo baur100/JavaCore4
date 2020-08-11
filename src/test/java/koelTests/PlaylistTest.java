d563946b9c9f7a0bec421f5a446821003ae22e61
Baurzhan <baurzhan.zh@gmail.com>
2020-08-10 23:52:58 -0400

    Logging

--- src/test/java/koelTests/PlaylistTest.java
+++ src/test/java/koelTests/PlaylistTest.java
@@ -1,11 +1,16 @@
 package koelTests;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageObjects.LoginPage;
 import pageObjects.MainPage;
 
+
+
 public class PlaylistTest extends BaseTest{
+    private static Logger logger = LogManager.getLogger();
 
     @Test
     public void playlistTest_loginToKoel() {
