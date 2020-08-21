47c3fcb5973e2772e5ed37f95ab1e187087ed759
Yana K <korentsovaiana@gmail.com>
2020-08-20 23:29:10 -0400

    Playlist Rename Tests hw

---
+++ src/test/java/koelTestsChromeYK/PlaylistRename.java
@@ -0,0 +1,32 @@
+package koelTestsChromeYK;
+
+import pageObjectsYK.LoginP;
+import pageObjectsYK.MainPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PlaylistRename extends BaseTest {
+
+
+    @Test
+    public void playlistRename() {
+        LoginP loginPage = new LoginP(driver);
+        loginPage.openLoginPage();
+        MainPage mainPage = loginPage.login("testpro.user04@testpro.io", "te$t$tudent");
+        String playlistId = mainPage.createNewPlaylist("X1X!");
+        String name = mainPage.renamePlaylist(playlistId, "Y1Y");
+        Assert.assertEquals("Y1Y", name);
+    }
+    @Test(enabled=false)
+    public void playlistRename2() throws InterruptedException {
+        LoginP loginPage = new LoginP(driver);
+        loginPage.openLoginPage();
+        MainPage mainPage = loginPage.login("testpro.user04@testpro.io", "te$t$tudent");
+        String playlistId = mainPage.createNewPlaylist("W1W!");
+        mainPage.renamePlaylist2(playlistId, "Z1Z");
+        Assert.assertTrue(mainPage.playlistRenamed(playlistId));
+        Thread.sleep(3000);
+
+
+    }
+}
