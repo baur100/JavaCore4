9c0a3644eb452a690ce5a750733e1cd653940c7f
Nina <nina.tuttle@yahoo.com>
2020-08-24 20:08:24 -0500

    HW 18

--- src/test/java/apiTests/HW18.java
+++ src/test/java/apiTests/HW18.java
@@ -1,6 +1,6 @@
 package apiTests;
 
-import io.restassured.internal.RestAssuredResponseOptionsGroovyImpl;
+
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import models.Category;
@@ -23,6 +23,79 @@ public class HW18 {
                         .statusCode(200)
                         .extract()
                         .response();
+
+        String res = response.asString();
+        System.out.println(res);
+    }
+
+    @Test
+    public void getPetById1() {
+        Response response1 =
+                given()
+                        .baseUri("https://petstore.swagger.io/v2/")
+                        .basePath("pet/250")
+                        .when()
+                        .get()
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+
+        String res1 = response1.asString();
+        System.out.println(res1);
+
+
+    }
+
+    @Test
+    public void getPetById2() {
+        Response response2 =
+                given()
+                        .baseUri("https://petstore.swagger.io/v2/")
+                        .basePath("pet/275")
+                        .when()
+                        .get()
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+
+        String res2 = response2.asString();
+        System.out.println(res2);
+
+    }
+
+    @Test
+    public void getPetById3() {
+        Response response3 =
+                given()
+                        .baseUri("https://petstore.swagger.io/v2/")
+                        .basePath("pet/275")
+                        .when()
+                        .get()
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+
+        String res3 = response3.asString();
+        System.out.println(res3);
+    }
+    @Test
+    public void getPetById4() {
+        Response response4 =
+                given()
+                        .baseUri("https://petstore.swagger.io/v2/")
+                        .basePath("pet/300")
+                        .when()
+                        .get()
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+
+        String res4 = response4.asString();
+        System.out.println(res4);
     }
 
 }
