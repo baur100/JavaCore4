69d9c73335310ff65e33e4707fae3edd229b27db
Baurzhan <baurzhan.zh@gmail.com>
2020-09-02 23:01:06 -0400

    Lesson 11 db calls

---
+++ src/test/java/models/PSongs.java
@@ -0,0 +1,11 @@
+package models;
+
+public class PSongs {
+    public String title;
+    public double length;
+
+    public PSongs(String title, double length) {
+        this.title = title;
+        this.length = length;
+    }
+}
